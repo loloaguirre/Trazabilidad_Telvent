98eda2ab661af53d6bd0b75528deadc4b61212bd
lolo_aguirre <lolo_aguirre@hotmail.com>
2015-10-16 00:16:59 +0200

    Initial commit

---
+++ app/src/androidTest/java/com/sosein/lolo/trazabilidadtelvent/ApplicationTest.java
@@ -0,0 +1,13 @@
+package com.sosein.lolo.trazabilidadtelvent;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
